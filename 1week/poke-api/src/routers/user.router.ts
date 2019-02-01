824769910badef1f77c35c9f3bb810d8eb4c9308
dfeli014 <dfeli014@fiu.edu>
2019-02-01 14:32:44 -0500

    added console.log()

--- 1week/poke-api/src/routers/user.router.ts
+++ 1week/poke-api/src/routers/user.router.ts
@@ -27,29 +27,29 @@ userRouter.get('',
   });
 
 // /users/:id - find by id
-userRouter.get('/:id', async (req, res) => {
-  console.log(req.params);
-  const idParam = +req.params.id;
-  // +'1' - will convert to number
-  // const user = users.find(ele => ele.id === idParam);
-  try {
-    const user = await UserDao.findById(idParam);
-    res.json(user);
-  } catch (err) {
-    console.log(err);
-    res.sendStatus(500);
-  }
-});
+// userRouter.get('/:id', async (req, res) => {
+//   console.log(req.params);
+//   const idParam = +req.params.id;
+//   // +'1' - will convert to number
+//   // const user = users.find(ele => ele.id === idParam);
+//   try {
+//     const user = await UserDao.findById(idParam);
+//     res.json(user);
+//   } catch (err) {
+//     console.log(err);
+//     res.sendStatus(500);
+//   }
+// });
 
-userRouter.post('', async (req, res) => {
-  // users.push(req.body);
-  try {
-    const user = await UserDao.save(req.body);
-    res.status(201);
-    res.json(user);
-  } catch (err) {
-    console.log(err);
-    res.sendStatus(500);
-  }
-});
+// userRouter.post('', async (req, res) => {
+//   // users.push(req.body);
+//   try {
+//     const user = await UserDao.save(req.body);
+//     res.status(201);
+//     res.json(user);
+//   } catch (err) {
+//     console.log(err);
+//     res.sendStatus(500);
+//   }
+// });
 
