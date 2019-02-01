a39b00a4e32b48c3c899c4a5c093a686b14773ba
dfeli014 <dfeli014@fiu.edu>
2019-02-01 14:31:02 -0500

    fixed userDao

--- 1week/poke-api/src/models/user.ts
+++ 1week/poke-api/src/models/user.ts
@@ -1,13 +1,9 @@
 export class User {
   id: number;
   username: string;
-  password: string;
-  name: string;
 
   constructor(id = 0, username = '', password = '', name = '') {
     this.id = id;
     this.username = username;
-    this.password = password;
-    this.name = name;
   }
 }
